5969fdf4bb608cf49fd716922829219415c9ed21
noranh121 <noranh@post.bgu.ac.il>
2024-10-04 23:29:13 +0300

    node.js

---
+++ Web Development Projects/Backend/2.3 NPM/index.js
@@ -0,0 +1,11 @@
+// var generateName = require('sillyname');
+
+// import generateName from "sillyName";
+// var sillyName = generateName();
+
+// console.log(`my name is ${sillyName}.`);
+
+import {randomSuperhero} from "superheroes";
+const name = randomSuperhero();
+
+console.log(`I am ${name}!`);
